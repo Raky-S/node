bcc471e29730a2f91c0fb25e5580461d3793d192
Raky-S <rakysy1@gmail.com>
2020-10-29 23:55:13 +0100

    du 29-10-20 un

--- express_authors/index.js
+++ express_authors/index.js
@@ -3,19 +3,72 @@ const port = 3000;
 
 const app = express();
 
-const authorsArr= ['Raky Sy, FR', 'Lawrence Nowell, UK', 'William Shakespeare, UK', 'Charles Dickens, US', 'Oscar Wilde, UK'] ;
+const authorsArr = ['Raky Sy, FR', 'Lawrence Nowell, UK', 'William Shakespeare, UK', 'Charles Dickens, US', 'Oscar Wilde, UK'];
+const books = ['Angels of Shery-City', 'Beowulf', 'Hamlet, Othello, Romeo and Juliet, MacBeth', 'Oliver Twist, A Christmas Carol', 'The Picture of Dorian Gray, The Importance of Being Earnest'];
+
+
+const all=[
+  {
+    id: 0,
+    name: "Raky Sy",
+    nationality: "FR"
+},
+
+  {
+      id: 1,
+      name: "Lawrence Nowell",
+      nationality: "UK"
+  },
+
+  {
+      id: 2,
+      name: "William Shakespeare",
+      nationality: "UK"
+  },
+
+  {
+      id: 3,
+      name: "Charles Dickens",
+      nationality: "Us"
+  },
+
+  {
+      id: 4,
+      name: "Oscar Wilde",
+      nationality: "UK"
+  },
+  
+]
+
+
+
+
 
 
 app.get('/', (req, res) => {
-    res.send('Authors API');
-    console.log('Authors API')
+  res.send('Authors API');
+  console.log('Authors API')
 });
 
-
-app.get('/authors/:authorsArr', (req, res) => {
-    res.send(`Mes authors: ${authorsArr[req.params.authorsArr]}`)
-    console.log( );
+app.get('/authors/:id', (req, res) => {
+  res.send('Erreur!!! The author with the ID 12133 does not exist');
 });
+
+app.get('/json/authors/:id', (req, res) => {
+  res.json(all[req.params.id])
+
+});
+
+app.get('/authors/:authorsArr/', (req, res) => {
+  res.send(`Mes authors: ${authorsArr[req.params.authorsArr]}`)
+  console.log(authorsArr[req.params.authorsArr]);
+});
+
+app.get('/authors/:books/books', (req, res) => {
+  res.send(`Books: ${books[req.params.books]}`,)
+  console.log(books[req.params.books]);
+});
+
 
 // app.get('/authors/: 1/', (req, res) => {
 //     res.send('Lawrence Nowell, UK' + authorsArr[req.params.authorsArr]);
