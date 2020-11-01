ec738d9a0994fc0a5dc53d7446deb4125808746e
Raky-S <rakysy1@gmail.com>
2020-11-01 20:29:19 +0100

    du 01-10-20

---
+++ formulairesFront/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const exphbrs = require('express-handlebars');
+
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'handlebars');
+
+app.engine('handlebars', exphbrs({
+    layoutsDir: __dirname + 'views/'
+}))
+
+app.use(express.static('public'))
+
+app.get('/home', function (req, res) {
+    res.render('home', {
+        title: 'Form Front'
+    });
+})
+
+
+// parse application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+// parse application/json
+app.use(express.json());
+
+app.post('/form/signup', (req, res) => {
+    console.log('form parameter', req.body.username);
+});
+
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+})
