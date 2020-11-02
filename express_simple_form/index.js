30225a355bcd742a5072a12eb2f98e4df808927b
Raky-S <rakysy1@gmail.com>
2020-11-02 21:13:52 +0100

    02-11-20 soir

--- express_simple_form/index.js
+++ express_simple_form/index.js
@@ -1,26 +1,42 @@
 const express = require('express');
-const exphbrs = require('express-handlebars');
+const exphbrs = require('express-handlebars')
 const app = express();
-const port = 3000;
+const port = 3000
+const students = ['Jean', 'Binta', 'Agathe', 'Adil'];
+
+
 
 app.engine('handlebars', exphbrs({
     defaultLayout: false,
-    layoutsDir: __dirname + '/views'
+    layoutsDir: __dirname + "views/"
 }));
 
 app.set('view engine', 'handlebars');
 
-app.use(express.static('views'))
 
-app.get('/', (req,res) =>{
+app.get('/', (req, res) => {
     res.render('home', {
-        title: 'Welcome to express simple form'
+        title: 'Welcome to express simple form',
+        students: ['Jean', 'Binta', 'Agathe', 'Adil']
+
     });
 });
 
+
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.json());
 
-app.listen( port, () => {
-    console.log('Server started on port' + port);
+
+
+app.post('/students/add', (req, res) => {
+    var username = req.body.username;
+    students.push(username);
+    return res.send(`User ${username}  has been added successfully`);
+})
+
+
+
+app.listen(port, () => {
+    console.log('Server started on port: ' + port);
 });
