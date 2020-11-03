9ab2fffcdafc8ee97f0229312ec89e24c20e96b4
Raky-S <rakysy1@gmail.com>
2020-11-03 17:06:08 +0100

    03-11-20

---
+++ express_l10n/index.js
@@ -0,0 +1,22 @@
+const app = express();
+const exphbrs = require('express-handlebars')
+
+app.engine('handlebars', exphbrs({
+    defaultLayout: false,
+    layoutsDir: __dirname + "views/"
+}));
+
+app.set('view engine', 'handlebars');
+
+
+app.get('/:lang?', (req, res) => {
+    res.render('home', {
+        
+    });
+
+});
+
+
+app.listen(port, () => {
+    console.log('Server started on port: ' + port);
+});
